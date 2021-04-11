aabcbf464042b4bdc6b19ba9b21b180ed50db7eb
Elegy <77524789+xxelegyxx@users.noreply.github.com>
2021-04-10 23:34:16 -0400

    Add files via upload

---
+++ output/generateHTML.js
@@ -0,0 +1,30 @@
+// function generateHTML {
+//     return `
+//     <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+//     <link rel="stylesheet" href="styles.css">
+//     <title>My Team</title>
+// </head>
+// <body>
+//     <div class="jumbotron jumbotron-fluid bg-danger">
+//         <div class="container">
+//           <h1 class="display-4 text-light">My Team</h1>
+//         </div>
+//       </div>
+
+//     <div class="card-section">
+
+//     </div>
+
+
+// </body>
+// </html>
+//     `;
+// }
+
+// module.exports = generateHTML;

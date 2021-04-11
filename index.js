3e334364aba93ed399910fd4afad9f2cdd925f17
Elegy <77524789+xxelegyxx@users.noreply.github.com>
2021-04-10 23:39:50 -0400

    changing index with commented out unfinished section

---
+++ index.js
@@ -0,0 +1,105 @@
+// import modules -> Employee subclasses, fs, inquirer, path, page-template, questions
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const fs = require('fs');
+const inquirer = require('inquirer');
+var path = require('path');
+const managerquestions = require('./src/manager-questions');
+const engineerquestions = require('./src/engineer-question');
+const internquestions = require('./src/intern-questions');
+const options = require('./src/options');
+const cardGen = document.querySelector('#card-section')
+
+// create an array to hold all of our team members
+let team = [];
+
+// init function
+function init() {
+	// function to create a Manager
+	function managerFunc() {
+		// prompt user with questions needed to satisfy the input for a manager object 
+		inquirer.prompt(managerquestions).then((managerInput) => {
+			let manager = new Manager(
+				managerInput.name, 
+				managerInput.id, 
+				managerInput.email, 
+				managerInput.officeNum
+				);
+			team.push(manager);
+			console.log(team);
+			employee()
+		});
+		
+	}
+
+	function employee() {
+		inquirer.prompt(options).then((answer) => {
+		  if (answer.what == "Engineer") {
+			engineerFunc();
+		  } else if (answer.what == "Intern") {
+			internFunc();
+		  } else {
+			console.log(team)
+			function createCards(team) {
+				forEach(
+					
+				)
+			}
+			
+		  }
+		});
+	  }
+
+	function engineerFunc() {
+		console.log("engineer sucess");
+		inquirer.prompt(engineerquestions).then((engineerInput) => {
+			let engineer = new Engineer(
+				engineerInput.name, 
+				engineerInput.id, 
+				engineerInput.email, 
+				engineerInput.gitHub
+				);
+			team.push(engineer);
+			console.log(team);
+			employee()
+		});
+	}
+
+	function internFunc() {
+		console.log("intern sucess");
+		inquirer.prompt(internquestions).then((internInput) => {
+			let intern = new Intern(
+				internInput.name, 
+				internInput.id, 
+				internInput.email, 
+				internInput.officeNum
+				);
+			team.push(intern);
+			console.log(team);
+			employee()
+		});
+	}
+
+
+  managerFunc();
+
+}
+//UNFINISHED
+// function generateCard() {
+// 	cardGen.innerhtml += `
+// 	<div class="card" style="width: 18rem;">
+//             <div class="card-body">
+//             <h5 class="card-title bg-default">TITLE</h5>
+//             <p class="card-text">ROLE</p>
+//         </div>
+//         <ul class="list-group list-group-flush">
+//             <li class="list-group-item">ONE</li>
+//             <li class="list-group-item">TWO</li>
+//             <li class="list-group-item">THREE</li>
+//         </ul>
+//     </div>`
+// };
+
+init()
